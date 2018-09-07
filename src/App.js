ecd29959067b381c364e4f3f132c4f98c67c168d
unknown <matt@unique.net>
2018-09-07 09:57:05 -0700

    intial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to the Cairs MetaData Tool!</h1>
+        </header>
+        <p className="App-intro">
+          <button>Schema MetaData</button>
+          <button>Menu MetaData</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
